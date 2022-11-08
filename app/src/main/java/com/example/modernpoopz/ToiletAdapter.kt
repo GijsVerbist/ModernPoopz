aed362ebdc12e444ddfbd91766bb85f8b182a44c
Gijs <gijsverbist@gmail.com>
2022-11-08 11:12:08 +0100

    map

--- app/src/main/java/com/example/modernpoopz/ToiletAdapter.kt
+++ app/src/main/java/com/example/modernpoopz/ToiletAdapter.kt
@@ -14,47 +14,40 @@ class ToiletAdapter(private val context: Context, private val dataSource: ArrayL
     private val inflater: LayoutInflater
     = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
-    //1
     override fun getCount(): Int {
         return dataSource.size
     }
 
-    //2
     override fun getItem(position: Int): Any {
         return dataSource[position]
     }
 
-    //3
     override fun getItemId(position: Int): Long {
         return position.toLong()
     }
 
-    //4
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        // Get view for row item
+
         val rowView = inflater.inflate(R.layout.list_item_toilet, parent, false)
 
-        // Get title element
+
         val titleTextView = rowView.findViewById(R.id.toilet_list_title) as TextView
 
-// Get subtitle element
+
         val subtitleTextView = rowView.findViewById(R.id.toilet_list_subtitle) as TextView
 
-// Get detail element
+
         val detailTextView = rowView.findViewById(R.id.toilet_list_detail) as TextView
 
-// Get thumbnail element
         val thumbnailImageView = rowView.findViewById(R.id.toilet_list_thumbnail) as ImageView
 
-        // 1
         val toilet = getItem(position) as Toilet
 
-// 2
         titleTextView.text = toilet.straat
         subtitleTextView.text = toilet.huisnummer
         detailTextView.text = toilet.postcode.toString()
 
-// 3
+
         //Picasso.with(context).load(toilet.imageUrl).placeholder(R.mipmap.ic_launcher).into(thumbnailImageView)
 
 
