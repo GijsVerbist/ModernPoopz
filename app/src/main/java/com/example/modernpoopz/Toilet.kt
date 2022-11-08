197d0b781366cf42883e6e8bace971b86eb6d759
Gijs <gijsverbist@gmail.com>
2022-11-08 09:19:23 +0100

    listviewPart1

---
+++ app/src/main/java/com/example/modernpoopz/Toilet.kt
@@ -0,0 +1,86 @@
+package com.example.modernpoopz
+
+import android.content.Context
+import org.json.JSONException
+import org.json.JSONObject
+
+
+class Toilet(
+            var straat: String,
+             var huisnummer: String,
+             var postcode: Int,
+             var betalend: String,
+             var categorie: String,
+             var omschrijving: String,
+             var doelgroep: String,
+             var luiertafel: String
+
+
+) {
+
+companion object{
+    fun getToiletsFromFile(filename: String, context: Context): ArrayList<Toilet>{
+        val toiletList = ArrayList<Toilet>()
+
+        try {
+            // Load data
+            val jsonString = loadJsonFromAsset(filename, context)
+            val json = JSONObject(jsonString)
+            val toilets = json.getJSONArray("features")
+
+            // Get Recipe objects from data
+            (0 until toilets.length()).mapTo(toiletList) {
+                Toilet(
+                    toilets.getJSONObject(it).getString("STRAAT"),
+                        toilets.getJSONObject(it).getString("HUISNUMMER"),
+                        toilets.getJSONObject(it).getInt("POSTCODE"),
+                        toilets.getJSONObject(it).getString("BETALEND"),
+                        toilets.getJSONObject(it).getString("CATEGORIE"),
+                        toilets.getJSONObject(it).getString("OMSCHRIJVING"),
+                        toilets.getJSONObject(it).getString("DOELGROEP"),
+                        toilets.getJSONObject(it).getString("LUIERTAFEL"))
+
+            }
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+
+
+
+        return toiletList
+    }
+
+    }
+}
+
+private fun loadJsonFromAsset(filename: String, context: Context): String? {
+    var json: String? = null
+
+    try {
+        val inputStream = context.assets.open(filename)
+        val size = inputStream.available()
+        val buffer = ByteArray(size)
+        inputStream.read(buffer)
+        inputStream.close()
+        json = String(buffer, Charsets.UTF_8)
+    } catch (ex: java.io.IOException) {
+        ex.printStackTrace()
+        return null
+    }
+
+    return json
+}
+
+
+/*
+class Toilet(var straat: String,
+             var huisnummer: String,
+             var postcode: Int,
+             var betalend: String,
+             var categorie: String,
+             var omschrijving: String,
+             var doelgroep: String,
+             var luiertafel: String) {
+
+
+}*/
